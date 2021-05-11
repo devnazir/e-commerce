9eb01abc7402b75ba0059f07f422ff5e5d2a94f8
Nazir Bahrul Ulum <nazirbachrululum@gmail.com>
2021-05-11 15:39:23 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/**/**/*.js"],
+  purge: ["./src/**/*.js", "./src/components/**/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
